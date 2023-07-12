d55f0257e5b0c6e11a087f3c6a1a12fe09359385
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-07-12 11:01:19 +0530

    world time

---
+++ lib/Pages/loading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.pmg', url: 'Europe/Berlin');
+    instance.getTime();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text("Loading..."),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/Pages/choose_location.dart';
+import 'package:world_time/Pages/home.dart';
+import 'package:world_time/Pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+));
+
